d1cdaa94d5dffbe9a10baeb8e8e7ef5f3b340ce0
xxyzll <xxyzll@yeah.net>
2022-07-26 21:20:58 +0800

    new start

---
+++ build/CMakeFiles/json_praser.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/json_praser.dir/main.cpp.o"
+  "json_praser.pdb"
+  "json_praser"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/json_praser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xx/桌面/json_prase_c++
+# Build directory: /home/xx/桌面/json_prase_c++/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
